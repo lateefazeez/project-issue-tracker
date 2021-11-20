673e80024805344baf700bb04d99e6731f24331c
Aman Hundal <64342261+KewlKewlKewl@users.noreply.github.com>
2021-11-20 01:30:01 +0000

    added axios call to retrieve JSON data from backend

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -7,19 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 puts "Seeding Data..."
-
-puts "Finding or Creating Projects"
-project1 = Project.find_or_create_by! title: 'Construction'
-project2 = Project.find_or_create_by! title: 'Software'
-
-puts "Creating Projects..."
-
-puts "Finding or Creating Users"
-user = User.find_or_create_by! first_name: 'Users'
-
 User.destroy_all
-
-user.create({
+User.create({
   first_name: "Barney",
   last_name: "Rubble",
   email: "barney@gmail.com",
@@ -27,8 +16,7 @@ user.create({
   phone_number: "555-555-5555",
   admin: true
 })
-
-user.create({
+User.create({
   first_name: "Fred",
   last_name: "Flinstone",
   email: "fred@gmail.com",
@@ -36,33 +24,28 @@ user.create({
   phone_number: "555-555-5544",
   admin: false
 })
-
-
 Project.destroy_all
-
-project2.create!({
+Project.create!({
   title: "Project tracking app",
-  description: Faker::Hipster.paragraph(3),
+  description: "hahahahahahahahahahahahahah",
   start_date: "Aug 19, 2021",
   end_date: "Nov 25, 2021",
   percentage_complete: 50,
   status: "on track",
   users_id: 1
 })
-
-project2.create!({
+Project.create!({
   title: "Scheduler app",
-  description: Faker::Hipster.paragraph(3),
+  description: "hahahahahahahahahahahahahah",
   start_date: "Jul 20, 2021",
   end_date: "Sept 20, 2021",
   percentage_complete: 100,
   status: "on track",
   users_id: 2
 })
-
-project2.create!({
+Project.create!({
   title: "Wikimap app",
-  description: Faker::Hipster.paragraph(3),
+  description: "hahahahahahahahahahahahahah",
   start_date: "Jul 14, 2021",
   end_date: "Oct 20, 2021",
   percentage_complete: 100,
@@ -70,3 +53,4 @@ project2.create!({
   users_id: 1
 })
 
+puts "Seeding Successful!..."
--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,16 @@
-
+import {useEffect} from 'react'
+import axios from 'axios'
 import './App.scss';
 
 function App() {
+  
+  useEffect(() => {
+    axios.get('http://localhost:3000/projects')
+    .then((data) => {
+      console.log(data)
+    })
+  }, [])
+  
   return (
     <div className="App">
     <h1>Hey Yo</h1>
