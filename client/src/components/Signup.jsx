3443b7bae1ea6f5b4e3a17340582227bdc3dffb8
Lateef Azeez <codelate18@gmail.com>
2021-11-22 17:49:58 +0000

    build the signup login

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,9 +1,47 @@
+import { useState } from "react";
 import PrimaryButton from "./PrimaryButton";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import GoogleIcon from '@mui/icons-material/Google';
+import { Button, Form, FormGroup, Input} from 'reactstrap';
+import Collapse from '@mui/material/Collapse';
 import './Signup.scss';
 
 const Signup = () => {
+  const [displayRegisterForm, setDisplayRegisterForm] = useState(false)
+  const [displayLoginForm, setDisplayLoginForm] = useState(false)
+
+  const registerUser = () => console.log("New User Registered")
+
+  const registerForm = (
+    <Form>
+        <FormGroup>
+          <Input className="form-inputs" type="email" name="email" id="email" placeholder="Enter your email" bsSize="lg" />
+        </FormGroup>
+        <FormGroup>
+          <Input className="form-inputs"  type="password" name="password" id="password" rows="5" placeholder="Password" bsSize="lg"/>
+        </FormGroup>
+        <FormGroup className="button-container">
+          <Button className="register_btn" onClick={registerUser}>Register</Button>
+        </FormGroup>
+      </Form>
+  )
+  const LoginForm = (
+    <Form>
+        <FormGroup>
+          <Input className="form-inputs" type="email" name="email" id="email" placeholder="Enter your email" bsSize="lg" />
+        </FormGroup>
+        <FormGroup>
+          <Input className="form-inputs"  type="password" name="password" id="password" rows="5" placeholder="Password" bsSize="lg"/>
+        </FormGroup>
+        <FormGroup className="button-container">
+          <Button className="register_btn" onClick={registerUser}>Register</Button>
+        </FormGroup>
+      </Form>
+  )
+
+  const handleFormDisplay = () => setDisplayRegisterForm(prev => !prev)
+  const handleLoginDisplay = () => setDisplayLoginForm(prev => !prev)
+
   return (
     <div className="page-container">
     <div className="opaque-background">
@@ -29,17 +67,25 @@ const Signup = () => {
             <h6>Continue with Google</h6>
           </div>
         </PrimaryButton >
+
         <PrimaryButton style={{ backgroundColor: "#262525", marginBottom: "20px", padding: "1rem 7rem", borderRadius: "0.4rem"}}>
-          <div className="signup-label">
-            <h6>Continue with your email</h6>
-          </div>
-        </PrimaryButton >
-        <PrimaryButton style={{ backgroundColor: "#262525", marginBottom: "20px", padding: "1rem 7rem", borderRadius: "0.4rem"}}>
-          <div className="signup-label">
-            <h6>Continue with your email</h6>
+          <div className="signup-label" onClick={handleFormDisplay}>
+            <h6>Register with your email</h6>
           </div>
         </PrimaryButton >
        </div>
+
+        <Collapse in={displayRegisterForm}>{registerForm}</Collapse>
+
+       <div className="login__header">
+         <h5>Already a user? Login below!</h5>
+       </div>
+       <PrimaryButton style={{ backgroundColor: "#262525", marginBottom: "20px", padding: "1rem 9rem", borderRadius: "0.4rem"}}>
+          <div className="signup-label" onClick={handleLoginDisplay}>
+            <h6>Login to your account</h6>
+          </div>
+        </PrimaryButton >
+        <Collapse in={displayLoginForm}>{LoginForm}</Collapse>
       
      </div>
     
